9c7679b9b53e7865ba6e2b0f4e681c716f7b063e
Rocamain <fjrocavazquez@gmail.com>
2022-05-02 15:47:57 +0100

    api end points changes

---
+++ src/api/dropdown-link/controllers/dropdown-link.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  dropdown-link controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::dropdown-link.dropdown-link');
