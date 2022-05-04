1819e14c94457172dee7dafebc9f0d655b65ec9c
Rocamain <fjrocavazquez@gmail.com>
2022-05-04 11:28:11 +0100

    changes in api for testing

--- src/api/dropdown-link/services/dropdown-link.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * dropdown-link service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::dropdown-link.dropdown-link');
---
+++ src/api/dropown-menu/services/dropown-menu.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * dropown-menu service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::dropown-menu.dropown-menu');
