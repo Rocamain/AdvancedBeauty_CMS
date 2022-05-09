13093b4971fa795f529da92c0147226625f4563d
Rocamain <fjrocavazquez@gmail.com>
2022-05-09 17:27:45 +0100

    creates cutom controller for hme and contact routes to pass te data in friendly way to client, modifies home component into a dynamic component

--- src/api/contact/controllers/contact.js
+++ src/api/contact/controllers/contact.js
@@ -1,9 +1,58 @@
-'use strict';
+"use strict";
 
 /**
  *  contact controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const COMPONENT = {
+  "home.carou": "Carousel",
+  "home-single-card-a": "SingleCardA",
+  "home-card": "Card",
+};
 
-module.exports = createCoreController('api::contact.contact');
+const { createCoreController } = require("@strapi/strapi").factories;
+
+module.exports = createCoreController("api::contact.contact", ({ strapi }) => {
+  return {
+    // Method 1: Creating an entirely custom action
+    async exampleAction(ctx) {
+      try {
+        ctx.body = "ok";
+      } catch (err) {
+        ctx.body = err;
+      }
+    },
+
+    // Method 2: Wrapping a core action (leaves core logic in place)
+    async find(ctx) {
+      // some custom logic here
+      ctx.query = { ...ctx.query, local: "en" };
+
+      // Calling the default core action
+      const { data, meta } = await super.find(ctx);
+
+      if (data.attributes.components) {
+        data.attributes.components.map((component) => {
+          component.componentName = COMPONENT[component.__component];
+        });
+      }
+      // some more custom logic
+      meta.date = Date.now();
+
+      return { data, meta };
+    },
+
+    // Method 3: Replacing a core action
+    async findOne(ctx) {
+      const { id } = ctx.params;
+      const { query } = ctx;
+
+      const entity = await strapi
+        .service("api::restaurant.restaurant")
+        .findOne(id, query);
+      const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+      return this.transformResponse(sanitizedEntity);
+    },
+  };
+});
--- src/api/home/controllers/home.js
+++ src/api/home/controllers/home.js
@@ -1,9 +1,57 @@
-'use strict';
+"use strict";
 
 /**
  *  home controller
  */
+const COMPONENT = {
+  "shared.carousel": "Carousel",
+  "home-single-card-a": "SingleCardA",
+  "shared.grid-a": "GridA",
+};
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::home.home');
+module.exports = createCoreController("api::home.home", ({ strapi }) => {
+  return {
+    // Method 1: Creating an entirely custom action
+    async exampleAction(ctx) {
+      try {
+        ctx.body = "ok";
+      } catch (err) {
+        ctx.body = err;
+      }
+    },
+
+    // Method 2: Wrapping a core action (leaves core logic in place)
+    async find(ctx) {
+      // some custom logic here
+      ctx.query = { ...ctx.query, local: "en" };
+
+      // Calling the default core action
+      const { data, meta } = await super.find(ctx);
+
+      if (data.attributes.components) {
+        data.attributes.components.map((component) => {
+          component.componentName = COMPONENT[component.__component];
+        });
+      }
+      // some more custom logic
+      meta.date = Date.now();
+
+      return { data, meta };
+    },
+
+    // Method 3: Replacing a core action
+    async findOne(ctx) {
+      const { id } = ctx.params;
+      const { query } = ctx;
+
+      const entity = await strapi
+        .service("api::restaurant.restaurant")
+        .findOne(id, query);
+      const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+      return this.transformResponse(sanitizedEntity);
+    },
+  };
+});
