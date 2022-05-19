9e419acd5e0a211ce3b5e55a48b4eff01196d4e0
Rocamain <fjrocavazquez@gmail.com>
2022-05-19 16:24:17 +0100

    commiting for testing redeployment

--- config/database.js
+++ config/database.js
@@ -1,3 +1,16 @@
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: "postgres",
+//     connection: {
+//       host: env("DATABASE_HOST", "127.0.0.1"),
+//       port: env.int("DATABASE_PORT", 5432),
+//       database: env("DATABASE_NAME", "db_a_b"),
+//       user: env("DATABASE_USERNAME", "db_user"),
+//       password: env("DATABASE_PASSWORD", "password"),
+//       ssl: env.bool("DATABASE_SSL", false),
+//     },
+//   },
+// });
 const parse = require("pg-connection-string").parse;
 const config = parse(process.env.DATABASE_URL);
 
