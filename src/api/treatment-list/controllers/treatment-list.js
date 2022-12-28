fdc5e19f27987e78c44d9e2d9011a7cbb907b7f6
Rocamain <fjrocavazquez@gmail.com>
2022-12-28 18:07:06 +0000

    feat(services): create services component
    Also  delete unnecesary components

--- src/api/treatment-list/controllers/treatment-list.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  treatment-list controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::treatment-list.treatment-list');
