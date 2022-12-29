f07d095cf4f6ba7f5eebf1d75dfcc90762b7dc2d
Rocamain <fjrocavazquez@gmail.com>
2022-12-29 13:09:24 +0000

    Adds console.log, to dedug on failed deployment

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -3,7 +3,7 @@ const parse = require("pg-connection-string").parse;
 const { host, port, database, user, password } = parse(
   process.env.DATABASE_URL
 );
-
+console.log({ host, port, database, user, password }, DATABASE_URL);
 module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
