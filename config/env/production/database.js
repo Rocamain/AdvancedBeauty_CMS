1089db2a5cdedfd66fa57ae3cd222fe0ce481f9b
Rocamain <fjrocavazquez@gmail.com>
2022-12-29 13:48:39 +0000

    downgrade strapi for node 16 and modifies plugin for env variable access

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -3,7 +3,7 @@ const parse = require("pg-connection-string").parse;
 const { host, port, database, user, password } = parse(
   process.env.DATABASE_URL
 );
-
+console.log(process.env);
 module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
