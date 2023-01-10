17c7ee29cd36c2f90736c682cdebee7649e2a2da
Rocamain <fjrocavazquez@gmail.com>
2023-01-10 15:47:57 +0000

    chores():adds images in login and favicon and upgrade packages

---
+++ src/admin/app.js
@@ -0,0 +1,22 @@
+import favicon from "./extensions/favicon.png";
+import logo from "./extensions/logo-cms.png";
+export default {
+  config: {
+    head: {
+      favicon: favicon,
+    },
+    auth: {
+      logo: logo,
+    },
+    menu: {
+      logo: favicon,
+    },
+    tutorials: false,
+    notifications: {
+      releases: false,
+    },
+  },
+  bootstrap(app) {
+    console.log(app);
+  },
+};
